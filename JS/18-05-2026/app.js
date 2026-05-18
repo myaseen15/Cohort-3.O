3109a9bf19076c513cae77d0055151e23a2eb9eb
myaseen15 <yaseen2409d@aptechsite.net>
2026-05-18 22:32:26 +0500

    Cohort 3.O

---
+++ JS/18-05-2026/app.js
@@ -0,0 +1,47 @@
+
+// console.log("This is normal text")
+// console.warn("This is warning")
+// console.error("This is Error")
+
+
+// variable 
+
+// variable are the container that used to store our data 
+
+
+ var a = 10;
+
+console.log(a);
+
+a = 20;
+
+console.log('value of a is ', a)
+console.log(a);
+
+// DataTypes 
+
+// - Premitive 
+
+// Number (10, 20.55, -10, 99999999)
+// String ('h', "Yaseen", 'Wellcome to my audience')
+  // Boolean ( true , false)
+  // undefined  whenever we declare a variable and not initialized then default value assigned by Js engine is undefine
+
+  // null (nothing, can be used to intentionally make variable )
+  // BigInt (number's range is 2^53-1 and beyound  that is BigInt)
+  // Symbol
+
+// - Non -Premitive (Reference) 
+    // - Array
+    // - Objects
+    // - Functions 
+
+    // var num = 30; ==> Number 
+
+    // alert("hello")
+    // confirm('Are you 18+')
+    
+    // var ans = confirm('This website for adult, Are You 18+ ?')
+    // console.log(ans);
+    
+  
